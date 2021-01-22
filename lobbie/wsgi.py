495d7acac9e82b901658781132ce5065b945674b
raja1802 <rajagandla16@gmail.com>
2021-01-22 18:01:18 +0530

    fclobby

--- lobbie/wsgi.py
+++ lobbie/wsgi.py
@@ -6,12 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-
-import os
-from whitenoise.django import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+import os
+from whitenoise import WhiteNoise
 
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lobbie.settings')
-
+django.setup()
 application = get_wsgi_application()
 application = WhiteNoise(application)
